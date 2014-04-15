1187ef9b135b88daa1fab7fc825af825c39961b6
Daniel Wroclawski <daniel@danwroclawski.com>
2014-04-14 20:28:43 -0400

    midterm changes

--- Dan_Wroclawski/Midterm/js/mulberrystreet.js
+++ Dan_Wroclawski/Midterm/js/mulberrystreet.js
@@ -31,12 +31,44 @@ $.getJSON(flickerAPI, {
   	format: "json"
 }).done(function(data) {
     $.each(data.items, function(i, item) {
-      	$("<a>", {"href":item.link})
+        $("<div>")
+        .append($("<a>", {"href":item.link}))
       	.append($("<img>").attr("src", item.media.m))
       	.appendTo("#flickrPhotos");
     });
 });
 
-$('#flickrPhotos').slick();
+  $('#flickrPhotos').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
 });
